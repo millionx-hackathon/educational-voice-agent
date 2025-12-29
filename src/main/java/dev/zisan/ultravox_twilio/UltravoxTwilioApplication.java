27321fb0314e2e729b2982438d04dfd1f7940f86
“abdulazizzisan” <binabdulaziz4983@gmail.com>
2025-12-30 00:31:57 +0600

    initial commit

---
+++ src/main/java/dev/zisan/ultravox_twilio/UltravoxTwilioApplication.java
@@ -0,0 +1,13 @@
+package dev.zisan.ultravox_twilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltravoxTwilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltravoxTwilioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zisan/ultravox_twilio/UltravoxTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zisan.ultravox_twilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltravoxTwilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
